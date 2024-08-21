d08503e25a61d99d4a2684455e68e5100139b283
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-20 21:52:27 -0300

    dev movies

---
+++ frontend/src/containers/Home/index.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header"
+
+function Home() {
+    return (
+        <>
+            <div>
+                <Header/>
+                <h1>Home</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
