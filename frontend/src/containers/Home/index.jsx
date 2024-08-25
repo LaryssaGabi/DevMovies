80190706c802b2dee427a74822f0580694023cff
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-25 11:38:51 -0300

    ajuste de rota

--- frontend/src/containers/Home/index.jsx
+++ frontend/src/containers/Home/index.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
 import api from '../../services/api'
-import { Backgroud, Container, Info,ContainerButtons, Poster } from './styles'
+import { Backgroud, Container, Info, ContainerButtons, Poster } from './styles'
 import Button from '../../components/Button'
+import Slider from '../../components/Slider'
+import { getImages } from '../../utils/getImages'
 
 function Home() {
     const [movie, setMovie] = useState([])
@@ -17,7 +19,7 @@ function Home() {
         async function getTopMovies() {
             const { data: { results } } = await api.get('/movie/top_rated')
             console.log(results)
-            setTopMovies(results[0])
+            setTopMovies(results)
         }
 
         getTopMovies()
@@ -31,7 +33,8 @@ function Home() {
 
         <>
             {movie && (
-                <Backgroud img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
+                <Backgroud img={getImages(movie.backdrop_path)}>
+
                     <Container>
                         <Info>
                             <h1>{movie.title}</h1>
@@ -42,12 +45,13 @@ function Home() {
                             </ContainerButtons>
                         </Info>
                         <Poster>
-                            <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+                            <img src={getImages(movie.poster_path)} />
                         </Poster>
                     </Container>
                 </Backgroud >
-            )
-            }
+            )}
+
+            {topMovies && <Slider info={topMovies} title={'Top Filmes'} />}
         </>
     )
 }
